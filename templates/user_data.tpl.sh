4f2aefc441f2f7bdd5373a8a9e39e575273da159
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-05-26 02:06:55 +0900

    feat: 不要だった stop-self-instance コマンドを削除
    OSの shutdown コマンドと同機能だったため

--- templates/user_data.tpl.sh
+++ templates/user_data.tpl.sh
@@ -7,15 +7,6 @@ logger [start user-data]
 perl -pi -e 's/^#?Port [0-9]+/Port ${ssh_port}/' /etc/ssh/sshd_config
 service sshd restart || service ssh restart
 
-# 自インスタンスの停止関数
-cat << 'EOS' >> /home/ec2-user/.bashrc
-function stop-self-instance() {
-  instanceId=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-  region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//'`
-  aws ec2 stop-instances --region $region --instance-ids $instanceId
-}
-EOS
-
 instanceId=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//'`
 
